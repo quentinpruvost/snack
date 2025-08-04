603b215e3e71cd47f9527aa58793c5cd183949fc
Quentin Pruvost <quentinbar26@gmail.com>
2025-08-04 12:45:45 +0200

    feat: Configure Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-vercel'; // <-- ON CHANGE L'IMPORT
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter() // On s'assure qu'il est bien appelé ici
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
